2bd81132fd0a82092154600dee6f93aa2b39305a
samsulhuda76 <61945288+samsulhuda76@users.noreply.github.com>
2020-07-01 22:14:30 +0700

    Oop

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.29326.143
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "TugasLab9_OOP_Collection", "TugasLab9_OOP_Collection\TugasLab9_OOP_Collection.csproj", "{28CF4C92-8B39-49EA-957D-38D9DB111C1B}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug | Any CPU
+		Release|Any CPU = Release | Any CPU
+	  EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{28CF4C92-8B39-49EA-957D-38D9DB111C1B}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{ 28CF4C92 - 8B39 - 49EA - 957D - 38D9DB111C1B}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{ 28CF4C92 - 8B39 - 49EA - 957D - 38D9DB111C1B}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{ 28CF4C92 - 8B39 - 49EA - 957D - 38D9DB111C1B}.Release|Any CPU.Build.0 = Release|Any CPU
+
+EndGlobalSection
+GlobalSection(SolutionProperties) = preSolution
+
+HideSolutionNode = FALSE
+
+EndGlobalSection
+GlobalSection(ExtensibilityGlobals) = postSolution
+
+SolutionGuid = { 4EAF28AB - 9078 - 4D4E-9DCD - DABC267FCFCE}
+EndGlobalSection
+EndGlobal
